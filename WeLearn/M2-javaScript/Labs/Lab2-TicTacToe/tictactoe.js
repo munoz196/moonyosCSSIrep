b406ea8fcb7a07c7b56a8b3708190f878084e0da
Jorge Munoz <munoz196@csusm.edu>
2019-07-25 16:14:44 -0700

    gvjg

---
+++ WeLearn/M2-javaScript/Labs/Lab2-TicTacToe/tictactoe.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('#1');
+const btn = document.querySelector('#2');
+const btn = document.querySelector('#3');
+const btn = document.querySelector('#4');
+const btn = document.querySelector('#5');
+const btn = document.querySelector('#6');
+const btn = document.querySelector('#7');
+const btn = document.querySelector('#8');
+const btn = document.querySelector('#9');
+
+
+.addEventListener('click', (e) => {
+  console.log("You clicked the red button!");
+  responseBox.style.backgroundColor = 'red'
+  responseBox.innerHTML = 'red';
+});
