9de4e8b70c55314840c150561fd8d57871c9e676
Jorge Munoz <munoz196@csusm.edu>
2019-07-25 12:08:05 -0700

    bgb

--- WeLearn/M2-javaScript/L4-JSLoop-DOM/loop/script.js
+++ WeLearn/M2-javaScript/L4-JSLoop-DOM/loop/script.js
@@ -2,6 +2,15 @@ const names = ['Alice','Bob','Charlie','Deborah','Evan'];
 
 
 
-names.forEach((name) => {
-  console.log(name);
+
+
+let sum=0;
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+
+const findTotal=((item) =>{
+for (i=0; i<10; i++){
+  sum = sum + item;}
+});
+numbers.forEach((findTotal) => {
+  console.log(sum);
 });
