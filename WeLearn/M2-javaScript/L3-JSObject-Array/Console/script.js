b0630b6d8033ff8603a5f57a77743be6ee44441c
Jorge Munoz <munoz196@csusm.edu>
2019-07-25 11:36:45 -0700

    idk

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,18 @@
+const article = {
+  name: 'Dog family gives birth to 10 puppies',
+  views: 1035,
+  datePublished: '03/25/2018',
+  author:{
+    name:'Joe Corgi',
+    title:'Senior Canine Editor'
+  }
+};
+
+const favoritefoods ={
+  food1: 'apples',
+  food2:'carrots',
+  food3:'pears',
+  food4:'peaches',
+  food5:'mango',
+};
+ const seasons = ['Spring','Summer','Fall','Winter']
---
+++ WeLearn/M2-javaScript/L4-JSLoop-DOM/loop/script.js
@@ -0,0 +1,7 @@
+const names = ['Alice','Bob','Charlie','Deborah','Evan'];
+
+
+
+names.forEach((name) => {
+  console.log(name);
+});
