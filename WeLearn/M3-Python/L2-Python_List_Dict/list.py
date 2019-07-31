f291bd6b209ead420e2115dfac6b9358ab65217a
Jorge Munoz <munoz196@csusm.edu>
2019-07-31 11:57:38 -0700

    Python list changes

---
+++ WeLearn/M3-Python/L2-Python_List_Dict/list.py
@@ -0,0 +1,23 @@
+# students = ["Alice","Javi","Damien","Javi"]
+# students.remove("Javi")
+# print(students)
+#
+# smith_siblings = ["Emily", "Monique","Giovanni","Jorge", "Jacob", "Emmanuel", "Hayden"]
+# for name in smith_siblings:
+#     print(name + " Smith")
+# print(len(smith_siblings))
+#
+# smith_siblings = ["Emily", "Monique","Giovanni","Jorge", "Jacob", "Emmanuel", "Hayden"]
+# for index in range(len(smith_siblings)):
+#     smith_siblings[index]=smith_siblings[index] + " Smith"
+# print(smith_siblings)
+
+superheroes= ["Captain Marvel", "Wonder Woman", "Storm","Kamala Khan", "Bumblebee","Jessica Jones"]
+print(superheroes[::2])
+# demoted_hero = str(raw_input("What superhero do you want to eliminate from the top 5?"))
+#
+# if demoted_hero in superheroes:
+#     superheroes.remove(demoted_hero)
+#     print("Top 5 Heroes:", superheroes)
+# else:
+#     print ("Hero not found.")
