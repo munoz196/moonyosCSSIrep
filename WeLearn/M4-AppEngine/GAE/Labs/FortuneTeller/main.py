689d1f4b9176dca331b758440f8dbb8dbbafb7b1
Jorge Munoz <munoz196@csusm.edu>
2019-07-31 15:54:31 -0700

    fortune teller lab

--- WeLearn/M4-AppEngine/GAE/Labs/FortuneTeller/main.py
+++ WeLearn/M4-AppEngine/GAE/Labs/FortuneTeller/main.py
@@ -60,7 +60,9 @@ class FortuneHandler(webapp2.RequestHandler):
         self.response.write(start_template.render())
 
     def post(self):
-        my_dict={}
+        random_fortune=get_fortune()
+        astro_sign = self.request.get('user_astrological_sign')
+        my_dict={'the_fortune': random_fortune, 'the_astro_sign': astro_sign }
         end_template=jinja_current_directory.get_template("templates/fortune_results.html")
         self.response.write(end_template.render(my_dict))
 
